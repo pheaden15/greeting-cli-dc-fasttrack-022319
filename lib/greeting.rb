7ec7a49030e40a3e302dd3de63f330bbae0d8473
Phillip Headen <pheaden15@gmail.com>
2019-02-24 15:32:19 +0000

    Automatically backed up by Learn

--- lib/greeting.rb
+++ lib/greeting.rb
@@ -1,6 +1,1 @@
 # code the #greeting method here!
-
-puts "Hello #{name}. It's mice to meet you."
-puts "Please enter your name so that we can greet you more personally."
-name = gets.strip
-greeting(name)
